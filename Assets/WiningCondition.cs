11467a163f69f88d0f1c50a9f28ca49bab14a189
Underwater008 <69733615+Underwater008@users.noreply.github.com>
2023-02-05 15:56:53 -0500

    Win imediately

--- Assets/WiningCondition.cs
+++ Assets/WiningCondition.cs
@@ -6,26 +6,36 @@ using UnityEngine.SceneManagement;
 public class WiningCondition : MonoBehaviour
 {
     Map map;
+    public ParticleSystem particle;
+    bool won = false;
     // Start is called before the first frame update
     void Start()
     {
         map = GameObject.Find("Map").GetComponent<Map>();
+        particle.Play();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Services.FolderManager.InfectedFolders.Count >= map.Folders.Count && !won)
+        {
+            won = true;
+        }
     }
-
+    void PlayPartical()
+    {
+        particle.Play();
+    }
     void OnTriggerEnter2D(Collider2D col)
     {
         if (col.gameObject.CompareTag("Player"))
         {
             Debug.Log("Trigger Huaman");
-            if (Services.FolderManager.InfectedFolders.Count >= map.Folders.Count)
+            if (Services.FolderManager.InfectedFolders.Count >= map.Folders.Count-1)
             {
                 SceneManager.LoadScene("Win Scene");
+
             }
 
         }
